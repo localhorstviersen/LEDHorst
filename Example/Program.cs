069f2862f06f9a591594ad79cc066cc5020ef410
Arwarld <marius.knabben@googlemail.com>
2018-06-09 13:17:23 +0200

    c# is done Python is left

---
+++ Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using LEDControl;
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LEDController ctrl = new LEDController();
+            LEDController.LEDStrip strip1 = new LEDController.LEDStrip(120, 13, 10, 255, 0, ctrl);
+            LEDController.LEDStrip strip2 = new LEDController.LEDStrip(16*16, 18, 11, 255, 1, ctrl);
+            strip1.Send();
+            for (byte i = 0; i < 120; i++)
+            {
+                strip1.Colors[i].R = 22;
+                strip1.Colors[i].G = i;
+                strip1.Colors[i].B = (byte)(i+120);
+            }
+            strip1.Send();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LEDControl/RGBColor.cs
@@ -0,0 +1,13 @@
+﻿namespace LEDControl
+{
+    public struct RGBColor
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+        public override string ToString()
+        {
+            return R.ToString("X2") + G.ToString("X2") + B.ToString("X2");
+        }
+    }
+}
